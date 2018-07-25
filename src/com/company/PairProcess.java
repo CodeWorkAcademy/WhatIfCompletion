6a66b82dd09b02fe981d156890091fccb9af6793
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-25 16:53:57 -0400

    Initial Commit

---
+++ src/com/company/PairProcess.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PairProcess {
+    public static void main(String[] args) {
+        int num1, num2;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print( "First Number? :" );
+        num1 = keyboard.nextInt();
+
+        System.out.print( "Last Number?  :" );
+        num2 = keyboard.nextInt();
+
+
+    }
+}
---
+++ src/com/company/PairProcessSolution.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PairProcessSolution {
+    public static void main(String[] args) {
+        int num1, num2, sum, product, average;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print( "First Number? :" );
+        num1 = keyboard.nextInt();
+
+        System.out.print( "Last Number?  :" );
+        num2 = keyboard.nextInt();
+
+        sum=num1+num2;
+        product=num1*num2;
+        average=sum/2;
+        System.out.println("Product="+product+" Sum="+sum+" Average="+average);
+
+        if(sum<1000){
+            System.out.print("~");
+        }else if(sum>200){
+            System.out.print("*");
+
+        }
+    }
+}
